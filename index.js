a66001f347ca82ea072a540241596d4c57edf833
katibur <katiburrahmansany@gmail.com>
2022-11-10 02:28:39 +0600

    delete api updated

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ async function run() {
         // services api
         app.get('/', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ time: -1 });
             const services = await cursor.limit(3).toArray();
             res.send(services);
         });
         app.get('/services', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ time: -1 });
             const services = await cursor.toArray();
             res.send(services);
         });
